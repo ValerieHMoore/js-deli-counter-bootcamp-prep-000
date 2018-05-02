71d05c0795564d19c974a1cede3db98792997d2c
Valerie Moore <periwinkle17@outlook.com>
2018-05-02 19:04:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,9 @@
  return "There is nobody waiting to be served!"
    }
 function currentLine(katzDeliLine) {
-    for (let i = 0; i < katzDeliLine.length; i++);{
+    var i = 0 
     while (katzDeliLine.length > 0) {
     return ("The line is currently: " + katzDeliLine[i] + ". " + katzDeliLine.shift())
   }
 return "The line is currently empty."
   }
-}
