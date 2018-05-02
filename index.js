b07c0fd5f6f75085d55c51ecb9385d57f1c89f28
Valerie Moore <periwinkle17@outlook.com>
2018-05-02 18:05:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,14 @@
   return katzDeliLine.length
  }
  function nowServing(katzDeliLine, name){
-   
+   while (katzDeliLine.length > 0) {
+     return katzDeliLine
+   }
  }
+ function whileLoop(n) {
+  let countdown = 100;
+  while (countdown > 0) {
+  console.log(--countdown)
+}
+return 'done'
+}
