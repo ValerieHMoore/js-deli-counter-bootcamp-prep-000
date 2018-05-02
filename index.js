9b51bd8605f8132737dceb6bf424cc13140e9a75
Valerie Moore <periwinkle17@outlook.com>
2018-05-02 16:35:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function takeANumber(katzDeliLine, name) {
  var katzDeli = [];
  for (let i = 0; i < katzDeliLine.length; i++){
-   katzDeli.push(katzDeliLine[i] + name[i])
+   katzDeli.push(katzDeliLine + name)
  }
  return katzDeli
 }
