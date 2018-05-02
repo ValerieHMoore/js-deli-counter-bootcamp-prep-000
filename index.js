f2db9c127cc31887dd032c922e84c8609534c001
Valerie Moore <periwinkle17@outlook.com>
2018-05-02 19:36:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function currentLine(katzDeliLine) {
     while (i < katzDeliLine.length) {
     newArray.push("The line is currently: " + [i] + ". " + katzDeliLine.shift());
     i++;
-  return newArray
     }
-  return "The line is currently empty."
-  }
+  return newArray
+}
