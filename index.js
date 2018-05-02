61bcc4e9d78b07529a20af82fb0e1ba44f092842
Valerie Moore <periwinkle17@outlook.com>
2018-05-02 16:43:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLine, name) {
  var katzDeli = []
- for (let i = 0; i < katzDeliLine.length; i++){
+ for (i = 0; i < katzDeliLine.length; i++){
    katzDeli.push("Welcome, " + name + ". You are number " + [i+1] "in line.")
  }
  return index + 1
