e66837519ed1dd7b17fb88129b718bdb673737fd
Valerie Moore <periwinkle17@outlook.com>
2018-05-02 19:22:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,22 +9,12 @@
  return "There is nobody waiting to be served!"
    }
 function currentLine(katzDeliLine) {
-for (let i = 0; i < 25; i++) {
-if(i)
-    }
     var i = 1; 
-    while (i < katzDeliLine.length) {
+    if (i < katzDeliLine.length) {
     newArray.push("The line is currently: " + [i] + ". " + katzDeliLine.shift())
     i++;
       }
-return "The line is currently empty."
+else {
+  return "The line is currently empty."
   }
-  
-function forLoop(array) {
-for (let i = 0; i < 25; i++) {
-if(i===1) {
-    array.push("I am 1 strange loop.")
-  } else {
-    array.push("I am " + i + "strange loops.")
-  }}
-  return array
+}
